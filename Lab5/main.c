0f98cc9bf6dc3e317791f47d63c670f69af70c12
yoopibit <yavorskyi.yurii@gmail.com>
2017-04-04 23:12:36 +0300

    lab5: add texture

--- Lab5/main.c
+++ Lab5/main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <GL/glut.h>
+#include <stdio.h>
 
 enum {TEAPOT = 1, QUIT};
 
@@ -7,6 +8,7 @@ static int spin = 0;
 static GLdouble angle;
 static int obj = TEAPOT;
 static int begin;
+static char *teapot_texture;
 
 
 void menu_select(int item)
@@ -33,6 +35,55 @@ void motion(int x, int y)
     glutPostRedisplay();
 }
 
+GLuint LoadBitmap( const char * filename, void **buffer,  int width, int height )
+{
+
+    if (width <= 0 || height <= 0) {
+        width = 1024;
+        height = 512;
+    }
+
+    GLuint texture;
+
+    unsigned char * data;
+
+    FILE * file;
+
+    file = fopen( filename, "rb" );
+
+    if ( file == NULL ) return 0;
+    data = (unsigned char *)malloc( width * height * 3 );
+    //int size = fseek(file,);
+    fread( data, width * height * 3, 1, file );
+    fclose( file );
+
+    for(int i = 0; i < width * height ; ++i)
+    {
+        int index = i*3;
+        unsigned char B,R;
+        B = data[index];
+        R = data[index+2];
+
+        data[index] = R;
+        data[index+2] = B;
+    }
+
+
+    glGenTextures( 1, &texture );
+    glBindTexture( GL_TEXTURE_2D, texture );
+    glTexEnvf( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE,GL_MODULATE );
+    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_LINEAR_MIPMAP_NEAREST );
+
+
+    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_LINEAR );
+    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,GL_REPEAT );
+    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T,GL_REPEAT );
+    gluBuild2DMipmaps( GL_TEXTURE_2D, 3, width, height,GL_RGB, GL_UNSIGNED_BYTE, data );
+    *buffer = data;
+
+    return texture;
+}
+
 void myinit(void)
 {
     glEnable(GL_LIGHTING);
@@ -40,6 +91,8 @@ void myinit(void)
 
     glDepthFunc(GL_LESS);
     glEnable(GL_DEPTH_TEST);
+
+    LoadBitmap("../Texture/tiles.bmp", &teapot_texture, 0, 0);
 }
 
 void display(void)
@@ -68,9 +121,11 @@ void display(void)
     glEnable(GL_LIGHTING);
     glPopMatrix();
 
-    if(obj == TEAPOT) {
-            glutSolidTeapot(1.0);
-    }
+    int width = 256;
+    int height = 256;
+
+    makeTexture(teapot_texture, width, height);
+    glutSolidTeapot(1.0);
 
     glPopMatrix();
     glPushAttrib(GL_ENABLE_BIT);
@@ -95,6 +150,22 @@ void myReshape(int w, int h)
     glMatrixMode(GL_MODELVIEW);
 }
 
+
+void makeTexture(char *buffer, int width, int height)
+{
+    glEnable(GL_TEXTURE_2D);
+    glPixelStorei(GL_UNPACK_ALIGNMENT,1);
+    glTexImage2D(GL_TEXTURE_2D,0,3,width,height,0,
+                 GL_RGB,GL_UNSIGNED_BYTE,buffer);
+    glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_CLAMP);
+    glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_CLAMP);
+    glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+    glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+    //glTexEnvf(GL_TEXTURE_ENV,GL_TEXTURE_ENV_MODE,GL_DECAL);
+    //glTexEnvf(GL_TEXTURE_ENV,GL_TEXTURE_ENV_MODE,GL_MODULATE);
+    glTexEnvf(GL_TEXTURE_ENV,GL_TEXTURE_ENV_MODE,GL_BLEND);
+}
+
 int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
@@ -102,6 +173,9 @@ int main(int argc, char **argv)
     glutInitWindowSize(500, 500);
     glutCreateWindow(argv[0]);
     myinit();
+
+
+
     glutMouseFunc(movelight);
     glutMotionFunc(motion);
     glutReshapeFunc(myReshape);
@@ -110,5 +184,9 @@ int main(int argc, char **argv)
     glutAddMenuEntry("Quit", QUIT);
     glutAttachMenu(GLUT_RIGHT_BUTTON);
     glutMainLoop();
+
+    if (teapot_texture)
+        free(teapot_texture);
+
     return 0;
 }
